552c3c9dffe96460b201d19ba2770cd3f7042fa9
Noorhan Issa <noorhanissa.94@gmail.com>
2025-06-20 01:34:45 +0300

    Delete frontend/src/utils/kebabCase.js

--- frontend/src/utils/kebabCase.js
+++
@@ -1,7 +0,0 @@
-export default function kebabCase(str) {
-  return str
-    .replace(/\s+/g, '-')          // Replace spaces with hyphens
-    .replace(/[A-Z]/g, (match) => '-' + match.toLowerCase()) // Convert camelCase to kebab-case
-    .replace(/^-/, '')             // Remove leading hyphen if present
-    .toLowerCase();                // Ensure lowercase
-}
